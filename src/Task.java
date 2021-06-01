8379fe2a1cb41557eb534010b97e561b8b422613
Meisam <meisamasady2002@gmail.com>
2021-06-01 02:42:24 -0700

    add Task

---
+++ src/Task.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Task {
+    public int level;
+    public HashMap<String,Integer>animalsAppearing=new HashMap<>();
+    public int firstStandardTime;
+    public int secondStandardTime;
+    public int firstBonus;
+    public int secondBonus
+    public int initialCoin;
+
+
+
+
+
+}
