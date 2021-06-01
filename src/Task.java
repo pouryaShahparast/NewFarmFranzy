2235ea54546fbc7fa0cdc4358509bdd9421811cd
Meisam <meisamasady2002@gmail.com>
2021-06-01 05:30:23 -0700

    edit

--- src/Task.java
+++ src/Task.java
@@ -9,9 +9,14 @@ public class Task {
     public int firstBonus;
     public int secondBonus;
     public int initialCoin;
-    public Task(int level,int firstStandardTime,int secondStandardTime,int firstBonus,int secondBonus,int initialCoin);
 
 
-
-
+    public Task(int level, int firstStandardTime, int secondStandardTime, int firstBonus, int secondBonus, int initialCoin) {
+        this.level = level;
+        this.firstStandardTime = firstStandardTime;
+        this.secondStandardTime = secondStandardTime;
+        this.firstBonus = firstBonus;
+        this.secondBonus = secondBonus;
+        this.initialCoin = initialCoin;
+    }
 }
