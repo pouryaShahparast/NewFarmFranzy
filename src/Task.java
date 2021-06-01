8688d3b413df3dca3793d11e7b2f064904d50c00
Meisam <meisamasady2002@gmail.com>
2021-06-01 03:20:09 -0700

    add Task edit

--- src/Task.java
+++ src/Task.java
@@ -3,12 +3,13 @@ import java.util.HashMap;
 public class Task {
     public int level;
     public HashMap<String,Integer>animalsAppearing=new HashMap<>();
+    public HashMap<String,Integer>neededCommodity=new HashMap<>();
     public int firstStandardTime;
     public int secondStandardTime;
     public int firstBonus;
-    public int secondBonus
+    public int secondBonus;
     public int initialCoin;
-
+    public Task(int level,int firstStandardTime,int secondStandardTime,int firstBonus,int secondBonus,int initialCoin);
 
 
 
