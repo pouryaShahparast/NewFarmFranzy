cd423f54961339523e0167a8547bc0341140ec96
Meisam <meisamasady2002@gmail.com>
2021-06-01 04:06:15 -0700

    add levels

--- src/Entrance.java
+++ src/Entrance.java
@@ -54,9 +54,104 @@ public class Entrance {
 
 
 
-        //
+        /*
+        declaring tasks of level from file missions.txt
+        file format:
+              int number of levels
+
+              @int level
+              int coins
+
+
+
+
+
+        */
+
+
+/*
+initializing tasks of levels
+ */
+        //level1
+        Task taskOfLevel1=new Task(1,50,60,400,300,200);
+        taskOfLevel1.animalsAppearing.put("bear",20);
+        taskOfLevel1.animalsAppearing.put("bear",35);
+
+        taskOfLevel1.neededCommodity.put("egg",5);
+        taskOfLevel1.neededCommodity.put("turkey",2);
+
+        //level2
+        Task taskOfLevel2=new Task(2,70,90,500,300,350);
+        taskOfLevel2.animalsAppearing.put("bear",15);
+        taskOfLevel2.animalsAppearing.put("bear",50);
+        taskOfLevel2.animalsAppearing.put("tiger",35);
+
+        taskOfLevel2.neededCommodity.put("egg",10);
+        taskOfLevel2.neededCommodity.put("turkey",5);
+        taskOfLevel2.neededCommodity.put("bread",5);
+        taskOfLevel2.neededCommodity.put("cow",2);
+        taskOfLevel2.neededCommodity.put("milk",2);
+        //level3
+        Task taskOfLevel3=new Task(3,90,110,600,700,400);
+        taskOfLevel3.animalsAppearing.put("bear",15);
+        taskOfLevel3.animalsAppearing.put("bear",70);
+        taskOfLevel3.animalsAppearing.put("bear",40);
+        taskOfLevel3.animalsAppearing.put("tiger",35);
+        taskOfLevel3.animalsAppearing.put("tiger",5);
+
+        taskOfLevel3.neededCommodity.put("egg",10);
+        taskOfLevel3.neededCommodity.put("turkey",5);
+        taskOfLevel3.neededCommodity.put("bread",5);
+        taskOfLevel3.neededCommodity.put("cow",3);
+        taskOfLevel3.neededCommodity.put("milk",5);
+        taskOfLevel3.neededCommodity.put("icevream",5);
+          //level4
+        Task taskOfLevel4=new Task(4,110,150,800,850,500);
+        taskOfLevel4.animalsAppearing.put("bear",5);
+        taskOfLevel4.animalsAppearing.put("bear",70);
+        taskOfLevel4.animalsAppearing.put("bear",40);
+        taskOfLevel4.animalsAppearing.put("tiger",35);
+        taskOfLevel4.animalsAppearing.put("tiger",90);
+        taskOfLevel4.animalsAppearing.put("lion",96);
+
+        taskOfLevel4.neededCommodity.put("egg",10);
+        taskOfLevel4.neededCommodity.put("turkey",5);
+        taskOfLevel4.neededCommodity.put("chicken",5);
+        taskOfLevel4.neededCommodity.put("bread",5);
+        taskOfLevel4.neededCommodity.put("cow",5);
+        taskOfLevel4.neededCommodity.put("milk",5);
+        taskOfLevel4.neededCommodity.put("icevream",5);
+        taskOfLevel4.neededCommodity.put("wool",5);
+        //level5
+        Task taskOfLevel5=new Task(5,150,200,1000,850,750);
+        taskOfLevel5.animalsAppearing.put("bear",5);
+        taskOfLevel5.animalsAppearing.put("bear",70);
+        taskOfLevel5.animalsAppearing.put("bear",40);
+        taskOfLevel5.animalsAppearing.put("bear",60);
+        taskOfLevel5.animalsAppearing.put("tiger",35);
+        taskOfLevel5.animalsAppearing.put("tiger",90);
+        taskOfLevel5.animalsAppearing.put("lion",96);
+        taskOfLevel5.animalsAppearing.put("lion",105);
+        taskOfLevel5.animalsAppearing.put("lion",120);
+
+        taskOfLevel5.neededCommodity.put("egg",20);
+        taskOfLevel5.neededCommodity.put("turkey",10);
+        taskOfLevel5.neededCommodity.put("chicken",8);
+        taskOfLevel5.neededCommodity.put("bread",5);
+        taskOfLevel5.neededCommodity.put("cow",5);
+        taskOfLevel5.neededCommodity.put("milk",5);
+        taskOfLevel5.neededCommodity.put("icevream",5);
+        taskOfLevel5.neededCommodity.put("wool",5);
+        taskOfLevel5.neededCommodity.put("flour",10);
+        taskOfLevel5.neededCommodity.put("feather",10);
+
+
+
+
 
         //start playing  Game
+
+
         System.out.println("*****************Enter your commands here*****************");
         String input="";
         Regex regex=new Regex();
@@ -116,6 +211,29 @@ public class Entrance {
             password=scanner.nextLine();
             fileWriter.write(userName+"\n"+password+"\n");
             fileWriter.close();
+            //create a file with username name
+            File userFile=new File("E:\\"+userName+".txt");
+            userFile.createNewFile();
+            FileWriter userFileWriter=new FileWriter(userFile);
+            /*file format:
+
+              int level:
+              int  coins
+
+
+            */
+
+            userFileWriter.write("1\\n\\200");
+            userFileWriter.close();
+
+
+
+
+
+
+
+
+
 
 
         } catch (IOException e) {
