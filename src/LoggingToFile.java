618588d0fd36ee893c75c5252b8e9bbc3a47cb2d
Meisam <meisamasady2002@gmail.com>
2021-06-04 02:10:12 -0700

    add logger

---
+++ src/LoggingToFile.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+public class LoggingToFile {
+
+    public  static FileHandler fileHandler;
+
+    {
+        try {
+            fileHandler = new FileHandler("logger.log",true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void logToFile(String message,String flag) {
+
+        Logger logger = Logger.getLogger("gamelogger");
+        logger.addHandler(fileHandler);
+        logger.setUseParentHandlers(false);
+        if(flag.equalsIgnoreCase("info"))
+        {
+            logger.info(message);
+        }
+         if(flag.equalsIgnoreCase("warning"))
+         {
+
+             logger.warning(message);
+
+         }
+         if(flag.equalsIgnoreCase("config"))
+         {
+             logger.config(message);
+         }
+
+
+    }
+
+
+
+
+}
