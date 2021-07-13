37925307d3b56555aac1410594b78bf68c0be614
Meisam <meisamasady2002@gmail.com>
2021-07-13 06:47:28 -0700

    add check if job is done

--- src/inGamGraphics/GameState.java
+++ src/inGamGraphics/GameState.java
@@ -3,7 +3,7 @@ package inGamGraphics;
 import GUI.Game;
 import model.*;
 import model.animals.*;
-import model.commodities.Commodity;
+import model.commodities.*;
 import model.factories.Factory;
 
 import java.awt.*;
@@ -33,8 +33,15 @@ public class GameState extends State{
         if(a % 100 == 0){
             turn++;
             addWildAnimals(turn);
-            longTick();
-            a = 0;
+            if(checkIfNeededIsPreparedReturnBoolean())
+            {
+                //stop game
+            }
+
+
+
+
+                        a = 0;
         }
     }
     public void init(){
@@ -203,8 +210,195 @@ public void addWildAnimals(int time)
 
 
 }
-    
-    
-    
-    
+ ///check 
+
+    public boolean checkIfNeededIsPreparedReturnBoolean()
+    {
+        boolean f1=true,f2=true,f3=true,f4=true,f5=true,f6=true,f7=true;
+        for (String string :
+                Game.task.neededCommodity.keySet()) {
+            if (string.equalsIgnoreCase("bread")) {
+                f1 = true;
+                int amountNeeded = Game.task.neededCommodity.get("bread");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof Bread)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f1 = true;
+                } else {
+
+                    f1 = false;
+
+                }
+
+            }
+
+
+            if (string.equalsIgnoreCase("cloth")) {
+                f2 = true;
+                int amountNeeded = Game.task.neededCommodity.get("cloth");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof Cloth)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f2 = true;
+                } else {
+
+                    f2 = false;
+
+                }
+
+            }
+            if (string.equalsIgnoreCase("egg")) {
+                f2 = true;
+                int amountNeeded = Game.task.neededCommodity.get("egg");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof Egg)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f2 = true;
+                } else {
+
+                    f2 = false;
+
+                }
+            }
+            if (string.equalsIgnoreCase("fabric")) {
+                f3 = true;
+                int amountNeeded = Game.task.neededCommodity.get("fabric");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof Fabric)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f3 = true;
+                } else {
+
+                    f3 = false;
+
+                }
+            }
+
+            if (string.equalsIgnoreCase("feather")) {
+                f3 = true;
+                int amountNeeded = Game.task.neededCommodity.get("feather");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof Feather)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f3 = true;
+                } else {
+
+                    f3 = false;
+
+                }
+            }
+            if (string.equalsIgnoreCase("flour")) {
+                f4 = true;
+                int amountNeeded = Game.task.neededCommodity.get("flour");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof Flour)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f4 = true;
+                } else {
+
+                    f4 = false;
+
+                }
+            }
+            if (string.equalsIgnoreCase("ice cream")) {
+                f5 = true;
+                int amountNeeded = Game.task.neededCommodity.get("ice cream");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof IceCream)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f5 = true;
+                } else {
+
+                    f5 = false;
+
+                }
+            }
+            if (string.equalsIgnoreCase("milk")) {
+                f6 = true;
+                int amountNeeded = Game.task.neededCommodity.get("milk");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof Milk)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f6 = true;
+                } else {
+
+                    f6 = false;
+
+                }
+            }
+            if (string.equalsIgnoreCase("pocket milk")) {
+                f7 = true;
+                int amountNeeded = Game.task.neededCommodity.get("pocket milk");
+                int amountExisting = 0;
+                for (Commodity commodity :
+                        storeroom.commodityHashSet) {
+                    if (commodity instanceof PocketMilk)
+                        amountExisting++;
+                }
+                if (amountExisting >= amountNeeded) {
+
+                    f7 = true;
+                } else {
+
+                    f7 = false;
+
+                }
+            }
+
+
+        }
+
+        return (f1&&f2&&f3&&f4&&f5&&f6&&f7);
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
